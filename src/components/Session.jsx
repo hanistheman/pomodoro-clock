cd2114691fa1baa0e178259d9706a89aa25d787c
hanistheman <gao647@purdue.edu>
2024-07-17 14:27:52 -0500

    Lifted a state to parent component and formatted moment durations using moment-duration-format

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,27 +1,23 @@
 import moment from 'moment';
  import React from 'react';
- import {useState} from 'react';
 
- const Session = () => {
-    const [sessionLength,  setSessionLength] = useState(60 * 25);
-    const decerementSessionLengthByOneMinute = () => {
-        const newSessionLength = sessionLength - 60;
-        if (newSessionLength < 0) {
-            setSessionLength(0);
-        } else {
-            setSessionLength(newSessionLength);
-        }
-    };
-    const incrementSessionLengthByOneMinute = () => {
-        setSessionLength(sessionLength + 60);
-    };
+
+ const Session = ({
+        sessionLength, 
+        decrementSessionLengthByOneMinute,
+        incrementSessionLengthByOneMinute,
+    }) => { 
     const sessionLengthInMinutes = moment.duration(sessionLength, 's').minutes(); 
     return (
         <div>
         <p id="session-label">Session</p>
         <p id = "session-length">{sessionLengthInMinutes}</p> 
-        <button id = "session-decrement" onClick = {decerementSessionLengthByOneMinute}>-</button>
-        <button id = "session-increment" onClick = {incrementSessionLengthByOneMinute}>+</button>
+        <button id = "session-decrement" onClick = {decrementSessionLengthByOneMinute}>
+            -
+        </button>
+        <button id = "session-increment" onClick = {incrementSessionLengthByOneMinute}>
+            +
+        </button>
         </div>
     )
  }
