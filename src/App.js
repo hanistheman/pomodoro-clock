231a7b522b880a275357e41d2a011fc1904890d8
hanistheman <gao647@purdue.edu>
2024-07-24 14:06:29 -0500

    Added reset button and an audio HTML tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 import Break from './components/Break';
 import Session from './components/Session';
 import TimeLeft from './components/TimeLeft';
 
 function App() {
-  const [breakLength,  setBreakLength] = useState(300); 
+    const audioElement = useRef(null);
+    const [currentSessionType, setCurrentSessionType] = useState('Session');
+    const[intervalID, setIntervalID] = useState(null);
+    const [sessionLength,  setSessionLength] = useState(60 * 25);
+    const [breakLength,  setBreakLength] = useState(300); 
+    const [timeLeft, setTimeLeft] = useState(sessionLength);
+    
+    // change our time left whenever sessionLength chages
+    useEffect(() => {
+        setTimeLeft(sessionLength);
+    }, [sessionLength]); 
+
     const decrementBreakLengthByOneMinute = () => {
         const newBreakLength = breakLength - 60;
         if (newBreakLength < 0) {
@@ -17,7 +28,6 @@ function App() {
     const incrementBreakLengthByOneMinute = () => {
         setBreakLength(breakLength + 60);
     };
-  const [sessionLength,  setSessionLength] = useState(60 * 25);
     const decrementSessionLengthByOneMinute = () => {
         const newSessionLength = sessionLength - 60;
         if (newSessionLength < 0) {
@@ -29,6 +39,54 @@ function App() {
     const incrementSessionLengthByOneMinute = () => {
         setSessionLength(sessionLength + 60);
     };
+    const isStarted = intervalID != null;
+    const handleStartStopClick = () => {
+        if (isStarted) {
+            // if we are in started mode, 
+            // we want to stop the timer
+            // clearInterval
+            clearInterval(intervalID);
+            setIntervalID(null);
+        } else {
+            // If we are in stopped mode, 
+            // decrement timeLeft byOne every second (1000 ms)
+            // to achieve this, setInterval will be used
+            const newIntervalID = setInterval(() => {
+                setTimeLeft(prevTimeLeft => {
+                    const newTimeLeft = prevTimeLeft - 1;
+                    if (newTimeLeft >= 0) {
+                        return prevTimeLeft - 1;
+                    }
+                    audioElement.current.play();
+                    if (currentSessionType == 'Session') {
+                        setCurrentSessionType('Break');
+                        setTimeLeft(breakLength);
+                    } else if (currentSessionType == 'Break') {
+                        setCurrentSessionType('Session');
+                        setTimeLeft(sessionLength);
+                    }
+                });
+            }, 100);
+        setIntervalID(newIntervalID);
+        }
+        
+    };
+    const handleResetButtonClick = () => {
+      // reset audio 
+      audioElement.current.load();
+      // clear timeout interval
+      clearInterval(intervalID);
+      // set intervalID to null
+      setIntervalID(null);
+      // set the sessionType to 'Session'
+      setCurrentSessionType('Session');
+      // reset the session length to 25 minutes
+      setSessionLength(60 * 25);
+      // reset the break length to 5 minutes
+      setBreakLength(60 * 5);
+      // reset timer to 25 minutes (original length)
+      setTimeLeft(60 * 25);
+    };
   return (
     <div className="App">
       <Break 
@@ -36,14 +94,21 @@ function App() {
         decrementBreakLengthByOneMinute = {decrementBreakLengthByOneMinute}
         incrementBreakLengthByOneMinute = {incrementBreakLengthByOneMinute}
       />
-      <TimeLeft sessionLength = {sessionLength} breakLength = {breakLength}
-
+      <TimeLeft  
+        handleStartStopClick = {handleStartStopClick}
+        timerLabel = {currentSessionType}
+        startStopButtonLabel = {isStarted ? 'Stop' : 'Start'}
+        timeLeft = {timeLeft}        
       />
       <Session 
         sessionLength = {sessionLength}
         decrementSessionLengthByOneMinute = {decrementSessionLengthByOneMinute}
         incrementSessionLengthByOneMinute = {incrementSessionLengthByOneMinute}
       /> 
+      <button id = "reset" onClick = {handleResetButtonClick}>Reset</button>
+      <audio id = "beep" ref = {audioElement}> 
+        <source src = "https://onlineclock.net/audio/options/default.mp3" type = "audio/mpeg"/>
+      </audio>
     </div>
   );
 }
--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,57 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
 
 momentDurationFormatSetup(moment);
 
-const TimeLeft = ({ breakLength, sessionLength }) => {
-    const [currentSessionType, setCurrentSessionType] = useState('Session');
-    const [timeLeft, setTimeLeft] = useState(sessionLength);
-    const[intervalID, setIntervalID] = useState(null);
-    // change our time left whenever sessionLength chages
-    useEffect(() => {
-        setTimeLeft(sessionLength);
-    }, [sessionLength]);
-
-    const isStarted = intervalID != null;
-    const handleStartStopClick = () => {
-        if (isStarted) {
-            // if we are in started mode, 
-            // we want to stop the timer
-            // clearInterval
-            clearInterval(intervalID);
-            setIntervalID(null);
-        } else {
-            // If we are in stopped mode, 
-            // decrement timeLeft byOne every second (1000 ms)
-            // to achieve this, setInterval will be used
-            const newIntervalID = setInterval(() => {
-                setTimeLeft(prevTimeLeft => {
-                    const newTimeLeft = prevTimeLeft - 1;
-                    if (newTimeLeft >= 0) {
-                        return prevTimeLeft - 1;
-                    }
-                    if (currentSessionType == 'Session') {
-                        setCurrentSessionType('Break');
-                        setTimeLeft(breakLength);
-                    } else if (currentSessionType == 'Break') {
-                        setCurrentSessionType('Session');
-                        setTimeLeft(sessionLength);
-                    }
-                });
-            }, 100);
-        setIntervalID(newIntervalID);
-        }
-        
-    };
-    
+const TimeLeft = ({ 
+    handleStartStopClick,  
+    startStopButtonLabel, 
+    timerLabel, 
+    timeLeft 
+}) => {   
 
     const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', {trim: false});
     return <div> 
     {formattedTimeLeft} 
-    <p id = 'timer-label'>{currentSessionType}</p>
+    <p id = 'timer-label'>{timerLabel}</p>
     <p id = 'time-left' >{formattedTimeLeft}</p>
-    <button onClick = {handleStartStopClick}> {isStarted? 'Stop' : 'Start'}</button>
+    <button id="start_stop" onClick = {handleStartStopClick}> 
+     {startStopButtonLabel} </button>
     </div>
 };
 
